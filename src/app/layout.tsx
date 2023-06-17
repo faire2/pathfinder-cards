a074af59f08125f14b50fa64961531663511a3a0
faire <faire.fionwe@gmail.com>
2023-06-17 19:48:50 +0200

    Add FFGood... fonts, remove original styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,5 @@
+import '../styles/fonts.css'
 import StyledComponentsRegistry from '@/utils/styledComponentsRegistry'
-import './globals.css'
-import { Inter } from 'next/font/google'
-
-
-const inter = Inter({ subsets: ['latin'] })
 
 
 export const metadata = {
@@ -19,7 +15,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
+			<body>
 				<StyledComponentsRegistry>
 					{children}
 				</StyledComponentsRegistry>
