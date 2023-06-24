438b7524b81e903ff05ea01a30c0b2a6694d82f1
faire <faire.fionwe@gmail.com>
2023-06-24 16:31:51 +0200

    Add sample spells

---
+++ src/data/spells.ts
@@ -0,0 +1,209 @@
+const electricArc: CardData = {
+	id: 'eaf43fac2b2e68bb25a9b4579',
+	tags: [],
+	name: 'Elktrický oblouk',
+	type: 'trik',
+	level: '1',
+	traits: 'trik, elektřina, zaklínání',
+	actions: 'aa',
+	body: 'Tradice arkána, duchovní, okultní\nSeslání (aa) (gesta, slova)\nDosah 30 stop; Cíle 1 nebo 2 stvoření\nZáchrana základní reflexy\n-\nOblouk elektrické energie skočí od jednoho cíle k dalšímu. Způsobíš elektrické poškození ve výši 1d4 plus tvůj bonus k sesílání.\n-\nZvýšení (+1) Poškození se zvyšuje o 1d4',
+	plain: 0,
+	meta: {
+		public: 0,
+		url: 'bZsCMhmw',
+	},
+}
+
+const light: CardData = {
+	id: 'eaf43fac2b2e68bb25a9b4579',
+	tags: [],
+	name: 'Světlo',
+	type: 'trik',
+	level: '1',
+	traits: 'trik, zaklínání, světlo',
+	actions: 'aa',
+	body: 'Tradice arkána, duchovní, okultní, prvotní\nSeslání (aa) (gesta, slova)\nDosah dotek; Cíle 1 předmět váhy 1 neno menší, buď opuštěný nebo držený tebou nebo souhlasícím spojencem\nTrvání do další přípravy kouzel\n-\nPředmět začne zářit, vyzařuje jasné světlo v dosahu 20 stop (a dalších 20 stop slabé světlo), podobně jako pochodeň. Pokud zakouzlíš stejné kouzlo na druhý předmět, kouzlo Světlo na prvním předmětu skončí.\n-\nZvýšení (+1) Poškození se zvyšuje o 1d4',
+	plain: 0,
+	meta: {
+		public: 0,
+		url: 'bZsCMhmw',
+	},
+}
+
+const produceFlame: CardData = {
+	id: 'e05e5707a5e0508b35acdbb4e',
+	tags: [],
+	name: 'Vytvoř plamen',
+	type: 'trik',
+	level: '1',
+	traits: 'útok, trik, zaklínání, oheň',
+	actions: 'aa',
+	body: 'Tradice arkána, prvotní\nSeslání (aa) (gesta, slova)\nDosah 30 stop; Cíle 1 stvoření\n-\nV tvé dlaní se objeví malá ohnivá koule, se kterou zaútočíš zblízka nebo na dálku. Proveď kouzelný útok proti obraně cíle. Normálně jde o útok zdálky, ale může na cíl zaútočit také zblízka, pokud je v dosahu tvého neozbrojeného útoku. Plamen způsobuje ohnivé poškození ve výši 1d4 + tvůj bonus k sesílání.\n\nKritický úspěch Cíl utrpí dvojnásobek poškození a 1d4 trvalého ohnivého poškození.\núspěch Cíl utrpí plné poškození.\n-\nZvýšení (+1) Poškození se zvyšuje o 1d4 a trvalé poškození při kritickém zásahu o 1d4.',
+	plain: 0,
+	meta: {
+		public: 0,
+		url: 'pdyvVwxj',
+	},
+}
+
+const rayOfFrost: CardData = {
+	id: 'e10630888fd25a4f6d384f91b',
+	tags: [],
+	name: 'Ledový paprsek',
+	type: 'trik',
+	level: '1',
+	traits: 'útok, trik, chlad, zaklínání',
+	actions: '',
+	body: 'Tradice arkána, prvotní\nSeslání (aa) (gesta, slova)\nDosah 120 stop; Cíle 1 stvoření\n-\nVystřelíš ledový paprsek. Hoď na útok kouzlem. Paprsek způsobuje chladné poškození ve výši 1d4 + tvůj bonus k sesílání.\n\nKritický úspěch Útok způsobí dvojnásobné poškození a cíl utrpí postih -10 ke své rychlosti, po dobu 1 kola.\núspěch Cíl utrpí normální poškození.\n-\nZvýšení (+1) Poškození se zvyšuje o 1d4',
+	plain: 0,
+	meta: {
+		public: 0,
+		url: 'tMyW4QTC',
+	},
+}
+
+const galeBlast: CardData = {
+	id: 'ec3d2aa9f3ad22bcb5dab8589',
+	tags: [],
+	name: 'Větrný úder',
+	type: 'trik',
+	level: '1',
+	traits: 'trik, vzduch, zaklínání',
+	actions: '',
+	body: 'Tradice arkána, prvotní\nSeslání (aa) (gesta, slova)\nZáchrana: Odolnost\n-\nZ tvých rukou proudí prudký vítr, který se kolem tebe obtáčí v 5-stopové emanaci. Každé stvoření v této oblasti utrpí tupé poškození ve výši tvého bonusu k sesílání, se záchranou na odolnost.\n\nKritický úspěch Stvoření je neovlivněno.\nÚspěch Strvoření utrpí poloviční poškození.\nNeúspěch Stvoření utrpí plné poškození a je zatlačeno 5 stop od tebe.\nKritické poškození Stvoření utrpí dvojnásobné poškození a je zatlačeno 10 stop od tebe.\n-\nZvýšení (+2) Poškození se zvyšuje o 1d6',
+	plain: 0,
+	meta: {
+		public: 0,
+		url: 'bOqHfbfq',
+	},
+}
+
+const animalAllies: CardData = {
+	id: 'ed58837b465d3423aa8816424',
+	tags: [],
+	name: 'Zvířecí spojenci',
+	type: 'kouzlo',
+	level: '1',
+	traits: 'vyvolávání',
+	actions: '',
+	body: 'Tradice prvotní\nSeslání (aa) (gesta, slova)\nOblast: emanace 5 stop\nZáchrana: základní reflexy\n-\nVyvoláš malá, běžná stvoření běžná v dané oblasti, jako je hmyz, ryby nebo ptáci, aby se rychle vrhli na blízké nepřátele. Stvoření zaútočí na cíle v oblasti a každému způsobí 3d4 bodavého poškození se záchranou na základní reflexy. \n-\nZvýšení (+1) Poškození se zvyšuje o 3d4',
+	plain: 0,
+	meta: {
+		public: 0,
+		url: 'H7fYzcSP',
+	},
+}
+
+export const heal: CardData = {
+	id: 'e4a8d51965fe5673802b11f2f',
+	tags: [],
+	name: 'Léčení',
+	type: 'kouzlo',
+	level: 1,
+	traits: 'léčení, nekromancie, pozitivní',
+	actions: '',
+	body: 'Tradice duchovní, prvotní\nSeslání (a) až (aaa)\nDosah různý, Cíle: 1 souhlasící stvoření nebo nemrtvý\n-\nNecháš sebou procházet proud pozitivní energie, který léčí živá stvoření a zraňuje nemrtvé. Pokud je cílem souhlasící živé stvoření, obnovíš mu 1d8 životů. Pokud je cílem nemrtvý, způsobíš mu stejné pozitivní poškození, se záchranou na základní Odolnost.\n\n(a) (gesta) Kouzlo má dosah dotknutí.\n(aa) (gesta, slova) Kouzlo má dosah 30 stop. Pokud léčíš živé stvoření, zvyš obnovené životy o 8.\n(aaa) (gesta, materiál, slova) Šíříš pozitivní energii v emanaci 30 stop. Zasáhnut je každý živý i neživý cíl v této oblasti.\n-\nZvýšení (+1) Množství léčení či poškození se zvyšuje o 1d8, a extra léčení pro seslání za 2 akce se zvyšuje o 8.',
+	plain: 0,
+	meta: {
+		public: 0,
+		url: 'J4lzctO6',
+	},
+}
+
+export const gustOfWind: CardData = {
+	id: 'efa9fca5ca0a5f0ee4f9463ad',
+	tags: [],
+	name: 'Poryv větru',
+	type: 'kouzlo',
+	level: '1',
+	traits: 'Vzduch, zaklínání',
+	actions: '',
+	body: 'Tradice arkána, prvotní\nSeslání (aa) (gesta, slova)\nOblast linie 60 stop\nZáchrana Odolnost; Trvání do začátku tvého dalšího tahu\n-\nZ tvé dlaně vyrazí prudký závan větru, ženoucí se z místa, kde jsi, na druhý konec linie.  Vítr uhasí malé nemagické ohně, rozežene mlhu, odfoukne předměty lehké váhy, a posune těžší předměty. Velké nebo menší stvoření v oblasti se musí pokusit o záchranu na Odolnost. Velké nebo menší stvoření, které později vkročí do oblasti větru, se musí při vkročení také pokusit o záchranu na Odolnost\n\nKritický úspěch Stvoření je neovlivněno.\nÚspěch Stvoření se nemůže pohybovat proti větru.\nNeúspěch Stvoření je sraženo k zemi. Pokud letělo, stanou se mu místo toho následky kritického neúspěchu.\nKritický neúspěch Stvoření je odsunuto 30 stop ve směru větru, sraženo k zemi a utrpí 2d6 tupého poškození.\n',
+	plain: 0,
+	meta: {
+		public: 0,
+		url: 'j6fsxm20',
+	},
+}
+
+export const airBubble: CardData = {
+	id: 'ee33420d529cdf3e678b0e30f',
+	tags: [],
+	name: 'Vzduchová bublina',
+	type: 'kouzlo',
+	level: '1',
+	traits: 'vyvolávání, vzduch',
+	actions: 'r',
+	body: 'Tradice arkána, duchovní, prvotní\nSeslání (r) (slova) Spouštěč: Stvoření v dosahu vstoupí do prostředí, ve kterém nedokáže dýchat.\nDosah: 60 stop; Cíle stvoření, které spustilo efekt\nTrvání: 1 minuta\n-\nKolem hlavy cíle se objeví bublina čistého vzduchu, která mu umožňuje normálně dýchat. Efekt skončí, jakmile se cíl vrátí do prostředí, ve kterém může normálně dýchat.',
+	plain: 0,
+	meta: {
+		public: 0,
+		url: 'M4fdd13S',
+	},
+}
+
+export const darkness: CardData = {
+	id: 'ec82b94912cbac7ba772954fd',
+	tags: [],
+	name: 'Temnota',
+	type: 'kouzlo',
+	level: '2',
+	traits: 'temnota, zaklínání',
+	actions: 'aaa',
+	body: 'Tradice arkána, duchovní, okultní, prvotní\nSeslání (materiál, pohyby, slova)\nDosah: 120 stop; Oblast ráz 20 ztop \nTrvání: 1 minuta\n-\nVytvoříš závoj temnoty, které zabraňuje světlo ve vstupu do i šíření v oblasti. Světlo nedokáže do oblasti vstoupit a jakékoliv nemagické zdroje světla, jako jsou pochodně nebo lucerny, v oblasti nevydávají žádné světlo, bez ohledu na to, jestli dosah jejich světla přesahuje hranici oblasti. Efekt také potlačí účinky kouzelného osvětlení, pokud je jeho úroveň nižší nebo rovná úrovni, na které je seslána temnota.  Světlo se nedokáže v oblasti šířit, takže stvoření v oblasti nedokážou vidět ven. Zvenku oblast vypadá jako koule naprosté temnoty.\n-\nZvýšení (4) Dokonce i stvoření s temnovizí (ale ne s vyšší temnovizí) v temnotě jen stěží vidí. Jejich cíle, na něž vidí skrze temnotu, jsou pro ně skryté.',
+	plain: 0,
+	meta: {
+		public: 0,
+		url: 'syhf54tS',
+	},
+}
+
+export const seaSurge: CardData = {
+	id: 'ee6d6b8ba4cd3f76dcb8ad395',
+	tags: [],
+	name: 'Mořská vlna',
+	type: 'kouzlo',
+	level: '2',
+	traits: 'nezvyklé, voda, zaklínání',
+	actions: 'aa',
+	body: 'Tradice arkána, prvotní\nSeslání (aa) (pohyby, slova)\nTrvání: do začátku tvého dalšího tahu\n-\nUdeříš nebo dupneš na blízký pevný nebo kapalný povrch, a vyšleš od sebe malou přílivovou vlnu. Vlna vytvoří plochou, 5 stop širokou a 10 stop vysokou stěnu vody, se šířkou mezi 5 až 15 stopami (dle tvé volby). Vlna se pohybuje po tomto povrchu 60 stop a po cestě uhasí všechny nemagické ohně. Odnese s sebou všechny nehlídané předměty váhy 1 nebo menší a odsune těžké předměty. Na začátku tvého dalšího tahu se vlna pohne stejným směrem o dalších 60 stop, a potom zmizí. Velká nebo menší stvoření nacházející se v oblasti, nebo které do oblasti vstoupí, se musí pokusit o záchranu na Odolnost.\n\nÚspěch Stvoření není ovlivněno.\nNeúspěch Stvoření je sraženo k zemi. Pokud stvoření Plavalo, utrží místo toho efekt kritického neúspěchu.\nKritický neúspech Stvoření je odstrčeno 20 stop ve směru pohybu vlny, je sraženo k zemi, a utrží 3d6 tupého poškození.\n',
+	plain: 0,
+	meta: {
+		public: 0,
+		url: '3sXjwHyr',
+	},
+}
+
+export const animalMessenger: CardData = {
+	id: 'e322be9b3390421fbcf47d6e8',
+	tags: [],
+	name: 'Zvířecí posel',
+	type: 'kouzlo',
+	level: '2',
+	traits: 'okouzlení, duševní',
+	actions: '',
+	body: 'Tradice prvotní\nSeslání 1 minuta (materiál, gesta, slova)\nDosah 120 stop\n-\nNabídneš dar jídla a drobné zvíře v dosahu kouzla se k tobě přiblíží, aby ho snědlo. Vtiskneš do jeho mysli obraz, směr a vzdálenost snadno nalezitelného místa, které dobře znáš. Volitelně můžeš ke zvířeti připevnit drobný předmět nebo vzkaz. Zvíře udělá vše, co je v jeho silách, aby dosáhlo cíle. Pokud se do cílové oblasti dostane, čeká až do vypršení trvání kouzla, a dovolí jakémukoliv ne-nepřátelskému stvoření se příblížit a odejmout připevněný předmět. Kouzlo končí po 24 hodinách nebo ve chvíli, kdy stvoření odstraní připevněný předmět, podle toho, co nastane dříve.\n\nPokud není v dosahu kouzla žádné drobné zvíře, je kouzlo ztraceno.',
+	plain: 0,
+	meta: {
+		public: 0,
+		url: '61Dh3dJl',
+	},
+}
+
+export const acidArrow: CardData = {
+	id: 'ea882e0ac4a932f58737f1aa7',
+	tags: [],
+	name: 'Kyselinový šíp',
+	type: 'kouzlo',
+	level: '2',
+	traits: 'kyselina, útok, zaklínání',
+	actions: 'aa',
+	body: 'Tradice arkána, prvotní\nSeslání (aa) (gesta, slova)\nDosah 120 stop; Cíle 1 stvoření\n-\nZaklínadlo vytvoří šíp z čisté kyseliny, který po zásahu dále poškozuje cíl. Proveď kouzelný útok. Zásah způsobí 3d8 kyselinového poškození a 1d6 trvalého kyselinového poškození. Kritický úspěch zdvojnásobí počáteční poškození, ale ne trvalé poškození.\n-\nZvýšení (+2) Poškození se zvyšuje o 2d8 a kyselinové poškození se zvyšuje o 1d6.',
+	plain: 0,
+	meta: {
+		public: 0,
+		url: '87OmCZ00',
+	},
+}
+
+export const saphireSpells = [electricArc, light, produceFlame, rayOfFrost, galeBlast, animalAllies, heal, gustOfWind, airBubble, darkness, seaSurge, animalMessenger, acidArrow]
