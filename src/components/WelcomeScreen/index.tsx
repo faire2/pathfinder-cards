017309688ab3fa43f9ff2b51e14df363f858e241
faire <faire.fionwe@gmail.com>
2023-09-17 07:14:41 +0200

    Update welcome screen text

--- src/components/WelcomeScreen/index.tsx
+++ src/components/WelcomeScreen/index.tsx
@@ -32,6 +32,6 @@ export default function WelcomeScreen({ onFinished }: Props) {
 
 const welcomeMessage = `Welcome to RPG card creator! You can use this page to import or create printable action cards for your spells, abilities, items - or anything else you can imagine.
 \n
-First you have to choose a name for your new project. Then you can either import cards in supported data format, or create them in this application.
+First you have to choose a name for your new project, then you can start creating the cards right away.
 
 `
